c3729b6056f45e010617649edb034db8ed2e023f
CristianLopez3 <cristian.c.lopez.m@gmail.com>
2025-01-02 19:24:14 -0500

    create order, email and notification service

---
+++ email-service/src/test/java/com/cristian/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
